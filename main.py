c1687833fad60f956818cc365d83558f309b3a03
Jossef Harush Kadouri <jossef12@gmail.com>
2023-05-10 05:50:32 +0300

    refactor: tidy up, removed irrelevant code

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import sys
 import dotenv
-
-sys.path.append('/home/ubuntu/.local/lib/python3.8/site-packages')
-
-dotenv.load_dotenv()
-
 import os
 import time
 import RPi.GPIO as GPIO
 import discord
+
+dotenv.load_dotenv()
 
 DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 client = discord.Client()
@@ -17,29 +14,16 @@ OUTPUT_PIN = 40
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(OUTPUT_PIN, GPIO.OUT)
 
-
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        print(
-            f'{client.user} is connected to the following guild:\n'
-            f'{guild.name}(id: {guild.id})\n'
-        )
-
-        members = '\n - '.join([member.name for member in guild.members])
-        print(f'Guild Members:\n - {members}')
-
-
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    response = "פתחתי"
+    response = '👍'
     try:
         open_garage()
     except Exception as e:
-        response = str(e)
+        response = f'⚠ {e}'
 
     await message.channel.send(response)
 
